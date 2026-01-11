28b9c85b8cfcf0d01930e3e771a47a236c08aaea
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-11 22:12:12 +0900

    猫の画像を表示

--- random-cat/app/page.tsx
+++ random-cat/app/page.tsx
@@ -1,13 +1,13 @@
 import { connection } from "next/server"; // 追加
+import { CatImage } from "./cat-image";
 import { fetchImage } from "./fetch-image"; // 追加
  
 export default async function Home() {
-  //           ^^^^^(1) asyncキーワードを追加
-  // (2) ビルド時にfetchImageの結果が固定されないようにする
+  // ビルド時にfetchImageの結果が固定されないようにする
   await connection();
-  // (3) APIから画像を取得
+  // APIから画像を取得
   const image = await fetchImage();
-  // (4) 画像URLをコンソールに表示
+  // 画像URLをコンソールに表示
   console.log("Home: 画像情報を取得しました", image);
-  return <div>猫画像予定地</div>;
+  return <CatImage url={image.url} />;
 }
