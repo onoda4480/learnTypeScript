ef9f590cf148951388f6cbece8e0ed958a02a523
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-11 21:55:20 +0900

    画像を取得する関数の実装

---
+++ random-cat/app/fetch-image.ts
@@ -0,0 +1,7 @@
+// APIから画像を取得する関数
+export async function fetchImage() {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log("fetchImage: 画像情報を取得しました", images);
+  return images[0]; // 画像情報の配列から最初の要素を返す
+}
