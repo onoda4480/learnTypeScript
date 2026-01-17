ddfaac2948bfd00efb2dbaed085ae9a5aa76cd3a
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-17 20:18:49 +0900

    APIキーを追加

--- random-cat/app/fetch-image.ts
+++ random-cat/app/fetch-image.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { CAT_API_KEY } from "./env";
 //画像情報の型定義
 type Image = {
     url: string;
@@ -7,7 +8,9 @@ type Image = {
 
 // APIから画像を取得する関数
 export async function fetchImage(): Promise<Image>{
-  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const res = await fetch("https://api.thecatapi.com/v1/images/search", {
+    headers: { "x-api-key": CAT_API_KEY },
+  });
   const images = await res.json();
   console.log("fetchImage: 画像情報を取得しました", images);
   return images[0]; // 画像情報の配列から最初の要素を返す
