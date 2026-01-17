1de14f1cfc062f428f213510e74cf193306c0170
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-17 21:43:07 +0900

    テストが通ることを確認

--- vitest-component-test-tutorial/like-button.browser.test.tsx
+++ vitest-component-test-tutorial/like-button.browser.test.tsx
@@ -1,8 +1,9 @@
-import { test } from "vitest";
+import { expect,test } from "vitest";
 import { render } from "vitest-browser-react"
 import { LikeButton } from "./like-button";
 
 test("ボタンを表示したときのカウントが999であること", async() => {
     const {getByRole} = await render(<LikeButton />);
     const button = getByRole("button");
+    await expect.element(button).toHaveTextContent(/^999$/);
 });
