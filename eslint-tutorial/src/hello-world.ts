0222f7b7e88fe03c1bdd068b0e20cebdb12d4a92
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-18 20:35:41 +0900

    cの定義が無いと警告される

--- eslint-tutorial/src/hello-world.ts
+++ eslint-tutorial/src/hello-world.ts
@@ -1,1 +1,10 @@
-export const helloWorld: any = "Hello World";
+type Choice = "A" | "B" | "C";
+
+export function func(choice: Choice) {
+  switch (choice) {
+    case "A":
+      break;
+    case "B":
+      break;
+  }
+}
