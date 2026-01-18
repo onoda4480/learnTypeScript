1ddc2dbb373cfc80d3bb1c94ab424060842941fd
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-18 20:16:58 +0900

    設定ファイルにルールの追加

--- eslint-tutorial/eslint.config.ts
+++ eslint-tutorial/eslint.config.ts
@@ -6,4 +6,10 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console":"warn",
+      camelcase: ["warn", {properties: "never" }],
+    },
+  }
 ]);
