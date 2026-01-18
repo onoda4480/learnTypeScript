2323c9542f2859457a310eda576a2e521e489124
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-18 20:28:30 +0900

    コードを自動修正するように追加

--- eslint-tutorial/eslint.config.ts
+++ eslint-tutorial/eslint.config.ts
@@ -2,14 +2,25 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser } 
+  },
   tseslint.configs.recommended,
   {
     rules: {
       "no-console":"warn",
       camelcase: ["warn", {properties: "never" }],
+      "@stylistic/semi": ["warn", "always"],
     },
   }
 ]);
